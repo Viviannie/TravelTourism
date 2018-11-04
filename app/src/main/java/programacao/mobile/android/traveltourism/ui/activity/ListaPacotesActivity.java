517e7e4c68bb02effb087b83d1bc9643a51c25c6
Viviannie <viviannie@gmail.com>
2018-11-04 14:35:14 -0300

    Primeiro commit

---
+++ app/src/main/java/programacao/mobile/android/traveltourism/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package programacao.mobile.android.traveltourism.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import programacao.mobile.android.traveltourism.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
